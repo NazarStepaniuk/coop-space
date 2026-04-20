d9b0c33f28426718119a3ec14d199f7f38cfac91
Nazar Stepaniuk <annastepaniuk77@gmail.com>
2026-04-20 19:35:59 +0200

    feat: add basic styles and change structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,20 @@
+import "./globals.scss";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+    subsets: ["latin", "cyrillic"],
+    display: "swap",
+    variable: "--font-montserrat",
+    weight: ["400", "500", "700"],
+});
+
 export default function RootLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
     return (
-        <html lang="uk">
+        <html lang="uk" className={montserrat.variable}>
             <body>{children}</body>
         </html>
     );
